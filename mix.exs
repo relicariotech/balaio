15eb9d1b55cf271f4a0549038b74e2ba126b5b2c
Dhony Silva <dhonysilva@Dhonys-MacBook-Air.local>
2023-07-24 20:49:07 -0300

    Replacing heroicons by icon function component

--- mix.exs
+++ mix.exs
@@ -50,8 +50,7 @@ defmodule Balaio.MixProject do
       {:telemetry_poller, "~> 1.0"},
       {:gettext, "~> 0.20"},
       {:jason, "~> 1.2"},
-      {:plug_cowboy, "~> 2.5"},
-      {:heroicons, "~> 0.5"}
+      {:plug_cowboy, "~> 2.5"}
     ]
   end
 
