455f92d587c930a303e3b51e4f2b00bc44dd7aff
Dhony Silva <dhonysilva@Dhonys-MacBook-Air.local>
2023-07-24 20:13:06 -0300

    Using the Function Component on the custom_component file

---
+++ lib/balaio_web/components/custom_components.ex
@@ -0,0 +1,19 @@
+defmodule BalaioWeb.CustomComponents do
+  use Phoenix.Component
+
+  def promo(assigns) do
+    ~H"""
+    <div class="promo">
+      <div class="deal">
+        <%= render_slot(@inner_block) %>
+      </div>
+      <div class="expiration">
+        Deal expires in <%= @expiration %> hours
+      </div>
+      <div class="legal">
+        <%= render_slot(@legal) %>
+      </div>
+    </div>
+    """
+  end
+end
--- lib/balaio_web/controllers/page_html.ex
+++ lib/balaio_web/controllers/page_html.ex
@@ -1,5 +1,7 @@
 defmodule BalaioWeb.PageHTML do
   use BalaioWeb, :html
 
+  import BalaioWeb.CustomComponents
+
   embed_templates "page_html/*"
 end
