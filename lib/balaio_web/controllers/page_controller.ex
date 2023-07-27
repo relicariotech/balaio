0c8ada959d429135cb95ccceba9089eafae84482
Dhony Silva <dhonysilva@Dhonys-MacBook-Air.local>
2023-07-27 00:56:41 -0300

    Enable list of Business on Home page

--- lib/balaio_web/controllers/page_controller.ex
+++ lib/balaio_web/controllers/page_controller.ex
@@ -4,6 +4,7 @@ defmodule BalaioWeb.PageController do
   def home(conn, _params) do
     # The home page is often custom made,
     # so skip the default app layout.
-    render(conn, :home, layout: false)
+    businesses = Balaio.Catalog.list_business()
+    render(conn, :home, layout: false, businesses: businesses)
   end
 end
