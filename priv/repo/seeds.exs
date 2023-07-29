a878d05ae928357442bd4b4c8b86fc20951dd995
Dhony Silva <dhonysilva@Dhonys-MacBook-Air.local>
2023-07-29 17:04:28 -0300

    Seeds for Categories

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -9,3 +9,18 @@
 #
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
+
+# Seed on Category table
+for title <- [
+      "Agência Bancária",
+      "Agência de Viagens",
+      "Turismo",
+      "Roupas e Calçados",
+      "Confecções",
+      "Engenharia",
+      "Pintura",
+      "Supermercado",
+      "Farmácia"
+    ] do
+  {:ok, _} = Balaio.Catalog.create_category(%{title: title})
+end
