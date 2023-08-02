3ec19cf87ef8532e1243f028f6fba2f49f35cfae
Dhony Silva <dhonysilva@Dhonys-MacBook-Air.local>
2023-08-01 22:09:26 -0300

    Id integer instead of UUID on BusinessCategories migration

--- priv/repo/migrations/20230729151835_create_business_categories.exs
+++ priv/repo/migrations/20230729151835_create_business_categories.exs
@@ -2,12 +2,13 @@ defmodule Balaio.Repo.Migrations.CreateBusinessCategories do
   use Ecto.Migration
 
   def change do
-    create table(:business_categories, primary_key: false) do
+    create table(:business_categories) do
+      # add :id, :binary_id, primary_key: true
       add :business_id, references(:business, on_delete: :delete_all, type: :binary_id)
       add :category_id, references(:categories, on_delete: :delete_all, type: :binary_id)
     end
 
-    create index(:business_categories, [:business_id])
+    # create index(:business_categories, [:business_id])
     create unique_index(:business_categories, [:category_id, :business_id])
   end
 end
