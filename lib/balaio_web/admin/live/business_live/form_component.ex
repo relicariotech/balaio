98c7c30930f7888ebc4c06991af910bf60a79d92
Dhony Silva <dhony.web@gmail.com>
2024-03-03 14:24:50 -0300

    apply list_categories function to list all of them

--- lib/balaio_web/admin/live/business_live/form_component.ex
+++ lib/balaio_web/admin/live/business_live/form_component.ex
@@ -3,7 +3,6 @@ defmodule BalaioWeb.Admin.BusinessLive.FormComponent do
 
   alias Balaio.Catalog
   alias Balaio.Catalog.BusinessCategory
-  alias Balaio.Catalog.Category
 
   @impl true
   def render(assigns) do
